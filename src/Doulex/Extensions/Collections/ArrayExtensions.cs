f2762af1d5d218ed2668af7902e5cbaf4204c2fc
nepton <nepton@qq.com>
2024-04-08 09:13:07 +0800

    publish 5.12 add ForEach in Array

--- src/Doulex/Extensions/Collections/ArrayExtensions.cs
+++ src/Doulex/Extensions/Collections/ArrayExtensions.cs
@@ -15,4 +15,56 @@ public static class ArrayExtensions
     {
         return source.Any() ? source : null;
     }
+
+    /// <summary>
+    /// For each item in the enumerable
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="T"></typeparam>
+    public static void ForEach<T>(this T[] source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            action(item);
+        }
+    }
+
+    /// <summary>
+    /// For each item in the enumerable
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="T"></typeparam>
+    public static void ForEach<T>(this T[] source, Action<T, int> action)
+    {
+        for (var i = 0; i < source.Length; i++)
+        {
+            action(source[i], i);
+        }
+    }
+
+    /// <summary>
+    /// For each item in the enumerable
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static Task ForEachAsync<T>(this T[] source, Func<T, Task> action)
+    {
+        return Task.WhenAll(source.Select(action));
+    }
+
+    /// <summary>
+    /// For each item in the enumerable
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static Task ForEachAsync<T>(this T[] source, Func<T, int, Task> action)
+    {
+        return Task.WhenAll(source.Select(action));
+    }
 }
