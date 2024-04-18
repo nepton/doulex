5985f135c43ad6e4700e22e2b9c85f9754cdeac6
nepton <nepton@qq.com>
2024-04-18 14:24:33 +0800

    publish 5.14 add MonitoringStream.cs to monitor the counter of stream

---
+++ src/Doulex/IO/MonitoringStream.cs
@@ -0,0 +1,119 @@
+﻿using Doulex.Performance;
+
+namespace Doulex.IO;
+
+/// <summary>
+/// A monitoring stream or called pass through stream, it used to intercept the data and calculate the data rate.
+/// </summary>
+public class MonitoringStream : Stream
+{
+    private readonly Stream _baseStream;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="baseStream"></param>
+    public MonitoringStream(Stream baseStream)
+    {
+        _baseStream = baseStream;
+    }
+
+    /// <summary>
+    /// The stats of the stream for data sent
+    /// </summary>
+    public Counter Sent { get; } = new(1000);
+
+    /// <summary>
+    /// The stats of the stream for data received
+    /// </summary>
+    public Counter Received { get; } = new(1000);
+
+    /// <summary>
+    /// Reset the counters
+    /// </summary>
+    public void ResetCounters()
+    {
+        Sent.Reset();
+        Received.Reset();
+    }
+
+    /// <inheritdoc />
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return _baseStream.Seek(offset, origin);
+    }
+
+    /// <inheritdoc />
+    public override void SetLength(long value)
+    {
+        _baseStream.SetLength(value);
+    }
+
+    /// <summary>When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.</summary>
+    /// <param name="buffer">An array of bytes. This method copies count bytes from buffer to the current stream.</param>
+    /// <param name="offset">The zero-based byte offset in buffer at which to begin copying bytes to the current stream.</param>
+    /// <param name="count">The number of bytes to be written to the current stream.</param>
+    /// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset">offset</paramref> and <paramref name="count">count</paramref> is greater than the buffer length.</exception>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="buffer">buffer</paramref> is null.</exception>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset">offset</paramref> or <paramref name="count">count</paramref> is negative.</exception>
+    /// <exception cref="T:System.IO.IOException">An I/O error occured, such as the specified file cannot be found.</exception>
+    /// <exception cref="T:System.NotSupportedException">The stream does not support writing.</exception>
+    /// <exception cref="T:System.ObjectDisposedException"><see cref="M:System.IO.Stream.Write(System.Byte[],System.Int32,System.Int32)"></see> was called after the stream was closed.</exception>
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _baseStream.Write(buffer, offset, count);
+
+        if (count > 0)
+        {
+            Sent.AddSample(count);
+        }
+    }
+
+    /// <summary>When overridden in a derived class, reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.</summary>
+    /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between offset and (offset + count - 1) replaced by the bytes read from the current source.</param>
+    /// <param name="offset">The zero-based byte offset in buffer at which to begin storing the data read from the current stream.</param>
+    /// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+    /// <returns>The total number of bytes read into the buffer. This can be less than the number of bytes requested if that many bytes are not currently available, or zero (0) if the end of the stream has been reached.</returns>
+    /// <exception cref="T:System.ArgumentException">The sum of <paramref name="offset">offset</paramref> and <paramref name="count">count</paramref> is larger than the buffer length.</exception>
+    /// <exception cref="T:System.ArgumentNullException"><paramref name="buffer">buffer</paramref> is null.</exception>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"><paramref name="offset">offset</paramref> or <paramref name="count">count</paramref> is negative.</exception>
+    /// <exception cref="T:System.IO.IOException">An I/O error occurs.</exception>
+    /// <exception cref="T:System.NotSupportedException">The stream does not support reading.</exception>
+    /// <exception cref="T:System.ObjectDisposedException">Methods were called after the stream was closed.</exception>
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        int bytesRead = _baseStream.Read(buffer, offset, count);
+
+        if (bytesRead > 0)
+        {
+            Received.AddSample(bytesRead);
+        }
+
+        return bytesRead;
+    }
+
+    /// <inheritdoc />
+    public override bool CanRead => _baseStream.CanRead;
+
+    /// <inheritdoc />
+    public override bool CanSeek => _baseStream.CanSeek;
+
+    /// <inheritdoc />
+    public override bool CanWrite => _baseStream.CanWrite;
+
+    /// <inheritdoc />
+    public override long Length => _baseStream.Length;
+
+    /// <inheritdoc />
+    public override long Position
+    {
+        get => _baseStream.Position;
+        set => _baseStream.Position = value;
+    }
+
+    /// <inheritdoc />
+    public override void Flush()
+    {
+        _baseStream.Flush();
+    }
+}
---
+++ src/Doulex/Performance/Counter.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics;
+
+namespace Doulex.Performance;
+
+/// <summary>
+/// Counter is used to count the data and the velocity of the count.
+/// </summary>
+public class Counter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="updateIntervalInMs">The latest velocity update interval in ms</param>
+    public Counter(int updateIntervalInMs)
+    {
+        _updateInterval = updateIntervalInMs;
+    }
+
+
+    /// <summary>
+    /// latest velocity value update interval (ms)
+    /// </summary>
+    private readonly int _updateInterval;
+
+    /// <summary>
+    /// Recent count
+    /// </summary>
+    private readonly Stopwatch _recentCountStopwatch = Stopwatch.StartNew();
+
+    /// <summary>
+    /// Total count
+    /// </summary>
+    private readonly Stopwatch _totalCountStopwatch = Stopwatch.StartNew();
+
+    /// <summary>
+    /// Recent count used to calculate the velocity
+    /// </summary>
+    private int _recentCount;
+
+    /// <summary>
+    /// Total count
+    /// </summary>
+    private int _totalCount;
+
+    /// <summary>
+    /// Latest count per second
+    /// </summary>
+    private int _latestCountPerSec;
+
+    /// <summary>
+    /// Total sample count
+    /// </summary>
+    public int Total => _totalCount;
+
+    /// <summary>
+    /// The total average velocity
+    /// </summary>
+    public int AverageVelocity
+    {
+        get
+        {
+            var elapsed = (int)_totalCountStopwatch.ElapsedMilliseconds;
+            return elapsed == 0 ? 0 : _totalCount * 1000 / elapsed;
+        }
+    }
+
+    /// <summary>
+    /// The latest velocity per second
+    /// </summary>
+    public int LatestVelocity
+    {
+        get
+        {
+            UpdateVelocity();
+            return _latestCountPerSec;
+        }
+    }
+
+    private void UpdateVelocity()
+    {
+        var elapsed = (int)_recentCountStopwatch.ElapsedMilliseconds;
+        if (elapsed <= _updateInterval)
+            return;
+
+        // calc
+        _latestCountPerSec = elapsed == 0 ? 0 : _recentCount * 1000 / elapsed;
+
+        // reset stat
+        _recentCount = 0;
+        _recentCountStopwatch.Restart();
+    }
+
+    /// <summary>
+    /// Add count to the stat
+    /// </summary>
+    /// <param name="count">Number of the count</param>
+    public void AddSample(int count)
+    {
+        UpdateVelocity();
+
+        Interlocked.Add(ref _totalCount,  count);
+        Interlocked.Add(ref _recentCount, count);
+    }
+
+    /// <summary>
+    /// Reset the stat
+    /// </summary>
+    public void Reset()
+    {
+        _totalCount  = 0;
+        _recentCount = 0;
+        _totalCountStopwatch.Restart();
+        _recentCountStopwatch.Restart();
+        _latestCountPerSec = 0;
+    }
+}
